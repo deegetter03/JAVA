539dd50f02f29f0c2b7e7d24c3af3ba45ccb4c06
Deepak Kumar <cryo3310@gmail.com>
2025-06-19 10:03:20 +0530

    java started

---
+++ DataTypes.java
@@ -0,0 +1,23 @@
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// Primitive data types in Java
+		System.out.println("Primitive Data Types in Java:");
+		char a = 'A';
+		byte b = 2;
+		short c = 22;
+		int d = 123;
+		float e = 5.123451f;
+		double f = 4.68295233532d;
+		boolean g = true;
+
+		System.out.println("char: " + a);
+		System.out.println("byte: " + b);
+		System.out.println("short: " + c);
+		System.out.println("int: " + d);
+		System.out.println("float: " + e);
+		System.out.println("double: " + f);
+		System.out.println("boolean: " + g);
+	}
+
+}
