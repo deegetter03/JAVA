97989c17583e7f760df491825e210d4e5e00c15b
Deepak Kumar <cryo3310@gmail.com>
2025-06-19 13:59:56 +0530

    more learning

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+        // This is a simple Java program that prints "Hello World!" to the console.
+		
+		//print()	Prints text without a new line at the end.EX-System.out.print("Hi") → Hi
+		//println()	Prints text with a new line at the end.EX- System.out.println("Hi") → Hi\n
+		//printf()	Prints formatted text (like in C). Doesn't add a new line by default.EX-System.out.printf("Hello %s", "John") → Hello John
+		
+		System.out.print("Hello World! ");
+		System.out.println("Hello World! "); // ln = next line
+		System.out.printf("Hello World! "); // print ,printf ,println
+	}
+}
